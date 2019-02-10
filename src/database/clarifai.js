f35801fd72d473dc150b3cef3473ee3bf7d45b1c
Amal Nazeem <amalnazeem@gmail.com>
2019-02-09 20:46:30 -0500

    updated clarifai api

--- src/database/clarifai.js
+++ src/database/clarifai.js
@@ -1,18 +1,40 @@
 const Clarifai = require('clarifai');
-const ClarConfig = require('../../Config').clarifai;
+const ClarConfig = require('../Config').clarifai;
 
 const ClarApp = new Clarifai.App(ClarConfig);
 
-const helperFunctions = {};
 /**
- * Returns a Promise that resolves into the all the tags for a given picture
+ * Returns a Promise that resolves into all the tags for a given picture via url
+ * Input:
+ *  imgUrl: String
+ * Output:
+ *  Promise(ArrayOfStrings)
  */
-helperFunctions.getTagsFromUrl = async (imgUrl) => {
+export const getTagsFromUrl = async (imgUrl) => {
   const model = await ClarApp.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"});
   const response = await model.predict(imgUrl);
   const concepts =  await response['outputs'][0]['data']['concepts'];
   const tags = concepts.map(concept => concept.name);
   return tags;
+}
+
+/**
+ * Returns a promise that resolves into all the tags for a given picture via base64Bytes
+ * Input:
+ *  base64Bytes: String
+ * Output:
+ *  Promise(ArrayOfStrings)
+*/
+export const getTagsFromImageFile = async (base64Bytes) => {
+  const model = await ClarApp.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"});
+  try {
+    const response = await model.predict(Clarifai.GENERAL_MODEL, { base64: base64Bytes});
+    const concepts =  await response['outputs'][0]['data']['concepts'];
+    const tags = concepts.map(concept => concept.name);
+    return tags;
+  } catch(err) {
+    console.log(err);
+  }
 }
 
 /**
@@ -27,12 +49,13 @@ helperFunctions.getTagsFromUrl = async (imgUrl) => {
  *  NOTE: If no match is found, { matchId: null, matchScore: 0 } will be returned;
  * 
  */
-helperFunctions.getBestMatchFromTags = (tagsOfOnePic, allTagsAllPics) => {
+export const getBestMatchFromTags = (tagsOfOnePic, allTagsAllPics) => {
   let bestMatch = null;
   let bestScore = 0;
   let origSet = new Set(tagsOfOnePic);
   allTagsAllPics.forEach( tagPair => {
-    let { id, tags } = tagPair;
+    let id = tagPair.id;
+    let tags = tagPair.tags;
     let possibleSet = new Set(tags);
     let intersection = new Set([...origSet].filter(x => possibleSet.has(x)));
     let score = intersection.size;
@@ -55,7 +78,7 @@ helperFunctions.getBestMatchFromTags = (tagsOfOnePic, allTagsAllPics) => {
  *  NOTE: If there aren't enough matches they will be { matchId: null, matchScore: 0 } each;
  * 
  */
-helperFunctions.getTopXMatchesFromTags = (tagsOfOnePic, allTagsAllPics, x) => { 
+export const getTopXMatchesFromTags = (tagsOfOnePic, allTagsAllPics, x) => { 
   let origSet = new Set(tagsOfOnePic);
   const tagsToScores = allTagsAllPics.map( tagPair => {
     let { id, tags }= tagPair;
@@ -76,8 +99,7 @@ helperFunctions.getTopXMatchesFromTags = (tagsOfOnePic, allTagsAllPics, x) => {
 
 
 
-
-module.exports = helperFunctions;
+export default { getTopXMatchesFromTags }
 /***
  * Usage Instructions:
  * const clarifai = require('./clarifai');
